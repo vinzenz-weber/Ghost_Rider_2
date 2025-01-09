02201ea59d37e9e8f7375a27cf2b5cfc745ad63b
vinzenz-weber <vinzenz.weber.ulm@gmail.com>
2025-01-09 14:42:25 +0100

    Player Health game logic
    Player health decreases when hitting object tagged with "Enemy"
    
    GameLogicScript with finite state machine added

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,7 +1,14 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public int health = 3;
+    public bool GameOver = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,21 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if(health <= 0) {
+            GameOver = true;
+        }
+        if(health > 0 ) {
+            GameOver = false;
+        }
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy") {
+            health -= 1;
+            Debug.Log("Health: " + health);
+        }
     }
 }
