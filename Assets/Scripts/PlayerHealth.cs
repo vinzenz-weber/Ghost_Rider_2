08c1bf79fb10f3bd493389285fccc51fc51eb3a0
vinzenz-weber <vinzenz.weber.ulm@gmail.com>
2025-01-23 13:09:40 +0100

    Bug Fix

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using Unity.VisualScripting;
-using UnityEditor.SearchService;
+//using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
